8b5e719c07ed012ca1bb2784cb37faccf082fa5b
smv7 <smv7.github@gmail.com>
2022-10-21 17:13:18 -0300

    Adding custom dtypes for Spacy's compatible format

--- src/custom_dtypes.py
+++ src/custom_dtypes.py
@@ -9,6 +9,11 @@ from typing import NewType, TypedDict
 
 # Types for Doccano's format.
 DoccanoLabelArray = NewType('DoccanoLabelArray', tuple[int, int, str])
-DoccanoJsonlDict = TypedDict('DoccanoJsonlDict', {'id': int, 'text': str, 'label': DoccanoLabelArray})
+DoccanoJsonlEntry = TypedDict('DoccanoJsonlEntry', {'id': int, 'text': str, 'label': DoccanoLabelArray})
 
-#TODO: Types for spaCy's compatible format.
+# Types for spaCy's compatible format.
+SpacyTokenDict = TypedDict('SpacyTokenDict', {'text': str, 'start': int, 'end': int, 'id': int})
+SpacySpanDict = TypedDict('SpacySpanDict', {'start': int, 'end': int,
+                                            'token_start': int, 'token_end': int, 'label': str})
+SpacyJsonlEntry = TypedDict('SpacyJsonlDict', {'text': str, 'tokens': list[SpacyTokenDict],
+                                               'spans': list[SpacySpanDict]})
