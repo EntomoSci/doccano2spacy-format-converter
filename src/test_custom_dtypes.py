4ca73a0cd72f0ffc708c588e05355b8a3141f2d4
smv7 <smv7.github@gmail.com>
2022-10-21 20:40:24 -0300

    Adding test case method's shells for custom dtype's integration tests

--- src/test_custom_dtypes.py
+++ src/test_custom_dtypes.py
@@ -17,11 +17,47 @@ class TestCustomDtypes(unittest.TestCase):
     Tests for the custom dtypes at `custom_dtypes.py` module."""
 
     def setUp(self) -> None:
-        doccano_jsonl_entry = DoccanoJsonlEntry()
-        doccano_jsonl_data = DoccanoJsonlData()
-        spacy_jsonl_token = SpacyJsonlToken()
-        spacy_jsonl_span = SpacyJsonlSpan()
-        spacy_jsonl_entry = SpacyJsonlEntry()
-        spacy_jsonl_data = SpacyJsonlData()
+        # doccano_jsonl_entry = DoccanoJsonlEntry()
+        # doccano_jsonl_data = DoccanoJsonlData()
+        # spacy_jsonl_token = SpacyJsonlToken()
+        # spacy_jsonl_span = SpacyJsonlSpan()
+        # spacy_jsonl_entry = SpacyJsonlEntry()
+        # spacy_jsonl_data = SpacyJsonlData()
+
+        return None
+
+    def test_doccano_jsonl_entry(self) -> None:
+        """
+        Test `custom_dtypes.DoccanoJsonlEntry`."""
+
+        return None
+
+    def test_doccano_jsonl_data(self) -> None:
+        """
+        Test `custom_dtypes.DoccanoJsonlData`."""
+
+        return None
+
+    def test_spacy_jsonl_token(self) -> None:
+        """
+        Test `custom_dtypes.SpacyJsonlToken`."""
+
+        return None
+
+    def test_spacy_jsonl_span(self) -> None:
+        """
+        Test `custom_dtypes.SpacyJsonlSpan`."""
+
+        return None
+
+    def test_spacy_jsonl_entry(self) -> None:
+        """
+        Test `custom_dtypes.SpacyJsonlEntry`."""
+
+        return None
+
+    def test_spacy_jsonl_data(self) -> None:
+        """
+        Test `custom_dtypes.SpacyJsonlData`."""
 
         return None
