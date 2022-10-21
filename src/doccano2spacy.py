460b3240be453b3b6fc2c7baa9b5a7279578c20e
smv7 <smv7.github@gmail.com>
2022-10-21 12:58:21 -0300

    Adding src file structures with docstrings and TODOs

---
+++ src/doccano2spacy.py
@@ -0,0 +1,18 @@
+"""
+Creation: 2022/10/21
+Author: https://github.com/smv7
+Description: Format converter from Doccano to Spacy's compatible format of Prodigy."""
+
+
+class Doccano2Spacy:
+    """
+    Format converter from Doccano to Spacy's compatible format of Prodigy."""
+
+    #TODO: Conversion rules as class attributes.
+
+    def __init__(self) -> None:
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ src/main.py
@@ -0,0 +1,18 @@
+"""
+Creation: 2022/10/21
+Author: https://github.com/smv7
+Description: Main script for Doccano/Spacy format conversions through a CLI."""
+
+
+import argparse
+
+
+#TODO: Taking file input with CLI.
+#TODO: Output file options.
+#TODO: Flags to control input/output formats.
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
