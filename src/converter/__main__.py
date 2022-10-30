522dcc7b39266044156694653fe0f0b5d4c1cb15
smv7 <smv7.github@gmail.com>
2022-10-30 01:21:28 -0300

    Updating CLI args and fixing bad converted file destination when package is installed

--- src/converter/__main__.py
+++ src/converter/__main__.py
@@ -17,8 +17,9 @@ def main():
     # Configuring the argument parser to control CLI.
     parser = argparse.ArgumentParser("d2s",
                                      description="Format converter from Doccano to Spacy's compatible format")
-    parser.add_argument('-f', '--file', help='File to convert.')
-    parser.add_argument('-o', '--out', default='./data/converted.jsonl', help='Converted file destination.')
+    parser.add_argument('file2convert', help='File to convert.')
+    parser.add_argument('destination', nargs='?', default='./converted.jsonl',
+                        help='Converted file destination. Defaults to "./converted.jsonl".')
 
     # Displaying the help message when no arguments are given.
     if len(sys.argv) == 1:
@@ -28,9 +29,9 @@ def main():
     args = parser.parse_args()
 
     # Reading the input file to convert and creating the output converted file.
-    destination_file_path = Path(__file__).parent.joinpath(args.out)
+    destination_file_path = Path.cwd().joinpath(args.destination)
     with destination_file_path.open('wt', encoding='utf-8') as file2write:
-        file2read = Path(args.file)
+        file2read = Path(args.file2convert)
         converter = Doccano2Spacy(file2read)
         converter.convert_jsonl_to(file2write)
 
